ab9d88874cb6529f08d834ccda9c388904886e34
ElayDadon <elaydad@gmail.com>
2024-05-24 18:12:17 +0300

    Add hash function to registration process to improve security. closes #12.

---
+++ code/Sadna-17-B/Utils/Cryptography.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.UI.HtmlControls;
+
+namespace Sadna_17_B.Utils
+{
+    public class Cryptography
+    {
+        public static string HashString(string str)
+        {
+            SHA256 sha256Hash = SHA256.Create() as SHA256;
+
+            byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+
+        }
+
+    }
+}
