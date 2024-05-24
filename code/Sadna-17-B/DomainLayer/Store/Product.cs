7fb8795b204de9748689719d8f5a1b32e92b4fb2
kopansE <evyatar.kopans@gmail.com>
2024-05-24 18:39:45 +0300

    implemented store product. closes #17

--- code/Sadna-17-B/DomainLayer/Store/Product.cs
+++ code/Sadna-17-B/DomainLayer/Store/Product.cs
@@ -7,5 +7,24 @@ namespace Sadna_17_B.DomainLayer.Store
 {
     public class Product
     {
+        private static int idCounter = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Category { get; set; }
+        public int CustomerRate { get; set; }
+        public string CustomerReview { get; set; } // up to 75 char
+        public string Description { get; set; } // up to 100 char
+
+        public Product(string name, float price, string category, int customerRate, string customerReview, string description = "")
+        {
+            Id = idCounter++;
+            Name = name.ToLower().Trim();
+            Price = price;
+            Category = category;
+            CustomerRate = customerRate;
+            CustomerReview = customerReview.Length <= 75 ? customerReview : customerReview.Substring(0, 75);
+            Description = description.Length <= 100 ? description : description.Substring(0, 100);
+        }
     }
 }
--- code/Sadna-17-B/DomainLayer/Store/Store.cs
+++ code/Sadna-17-B/DomainLayer/Store/Store.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+
+using Sadna_17_B.DomainLayer.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +10,56 @@ namespace Sadna_17_B.DomainLayer.Store
 {
     public class Store
     {
+        public string _name { get; set; }
+        public string _email { get; set; }
+        public string _phone_number { get; set; }
+        public string _store_description { get; set; }
+
+        public string _address { get; set; }
+        public Inventory _inventory { get; set; }
+
+        public Store(string name, string email, string phone_number, string store_description,
+                        string address, Inventory inventory)
+        {
+            // stores can be created via controller only
+            _name = name;
+            _email = email;
+            _phone_number = phone_number;
+            _store_description = store_description;
+            _address = address;
+            _inventory = inventory;
+        }
+
+
+        // Facade methods to control the inventory
+        public void AddProduct(Product product, int amount)
+        {
+            _inventory.AddProduct(product, amount);
+        }
+
+        public void RemoveProduct(string productName)
+        {
+            _inventory.RemoveProduct(productName);
+        }
+
+        public void ReduceProductAmount(string productName, int amount)
+        {
+            _inventory.ReduceProductAmount(productName, amount);
+        }
+
+        public int GetProductAmount(string productName)
+        {
+            return _inventory.GetProductAmount(productName);
+        }
+
+        public List<string> GetAllProductNames()
+        {
+            return _inventory.GetAllProductNames();
+        }
+
+        public Dictionary<string, int> GetAllProductsFromInventory()
+        {
+            return _inventory.GetAllProducts();
+        }
     }
 }
