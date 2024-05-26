841b45632821f57e50b95f5783677315e3d1b60c
omertarshish <omer.tarshish@gmail.com>
2024-05-27 00:32:56 +0300

    Implement support for all Store Manager operations according to his specific Manager Authorizations (5) close #33

---
+++ code/Sadna-17-B/DomainLayer/Store/PurchasePolicy.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sadna_17_B.DomainLayer.Store
+{
+    public class PurchasePolicy
+    {
+
+        public string PolicyName { get; set; }
+        public List<PurchaseType> AllowedPurchaseTypes { get; set; }
+
+
+        public void AllowPurchaseType(PurchaseType type)
+        {
+            AllowedPurchaseTypes.Add(type);
+        }
+
+        public void AddPurchaseType(PurchaseType type)
+        {
+            AllowedPurchaseTypes.Add(type);
+        }
+
+        public void RemovePurchaseType(PurchaseType type)
+        {
+            AllowedPurchaseTypes.Remove(type);
+        }
+    }
+
+
+    public abstract class PurchaseType
+    {
+        protected double price;
+        public abstract void executePurchase();
+    }
+
+
+    public class ImmediatePurchase : PurchaseType
+    {
+        public override void executePurchase()
+        {
+            //implement purchase logic
+
+        }
+    }
+
+    public class BidPurchase : PurchaseType
+    {
+        public double BidPrice { get; set; }
+        public override void executePurchase()
+        {
+            //implement purchase logic
+        }
+
+        public void submitBid(double bidPrice)
+        {
+            //implement bid logic
+        }
+
+        public void AcceptBid(double bidPrice)
+        {
+            //implement bid accept logic
+        }
+
+        public void RejectBid(double bidPrice)
+        {
+            //implement bid accept logic
+        }
+
+        public void CounterBid(double bidPrice)
+        {
+            //implement bid accept logic
+        }
+    }
+
+
+
+
+    public class AuctionPurchase : PurchaseType
+    {
+        public double StartingPrice { get; set; }
+        public double HighestBid { get; set; }
+        public string HighestBidder { get; set; }
+
+        public override void executePurchase()
+        {
+            //implement purchase logic
+        }
+    }
+
+    public class LutteryPurchase : PurchaseType
+    {
+        public double TotalAmountCollected { get; set; }
+        public Dictionary<string, double> Participants { get; set; }
+        public int WinningTicket { get; set; }
+        public override void executePurchase()
+        {
+            //implement purchase logic
+        }
+
+        public void EnterLuttery(string userName, double amount)
+        {
+            //implement luttery logic
+        }
+
+    }
+}
+
+    
