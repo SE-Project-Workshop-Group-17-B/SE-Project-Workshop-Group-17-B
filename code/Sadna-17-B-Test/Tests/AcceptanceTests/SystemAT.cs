f81e46f8fe6b70b3d704c3f4ab038b93363b59a3
Daniel Bril <brildan@post.bgu.ac.il>
2024-05-28 11:45:43 +0300

    Acceptence tests updates

--- code/Sadna-17-B-Test/Tests/AcceptanceTests/SystemAT.cs
+++ code/Sadna-17-B-Test/Tests/AcceptanceTests/SystemAT.cs
@@ -4,6 +4,7 @@ using Sadna_17_B.ServiceLayer;
 using Sadna_17_B.ServiceLayer.Services;
 using Sadna_17_B.ServiceLayer.ServiceDTOs;
 using Sadna_17_B.Utils;
+using Sadna_17_B.DomainLayer.StoreDom;
 
 namespace Sadna_17_B_Test.Tests.AcceptanceTests
 {
@@ -15,6 +16,14 @@ namespace Sadna_17_B_Test.Tests.AcceptanceTests
         UserDTO userDTO;
         string username1 = "test1";
         string password1 = "password1";
+
+        //for store test purposes
+        string name = "testStore";
+        string email = "testemail@post.bgu.ac.il";
+        string phonenumber = "0525381648";
+        string storeDescr = "test store for testing";
+        string addr = "Beer sheve BGU st.3";
+        Inventory inv = new Inventory();
 
         [TestInitialize]
         public void SetUp()
@@ -28,6 +37,8 @@ namespace Sadna_17_B_Test.Tests.AcceptanceTests
         [TestMethod]
         public void TestLogin()
         {
+            SetUp();
+
             Response res = userService.Login(username1, password1);
             Assert.IsTrue(res.Success);
             userDTO = res.Data as UserDTO;
@@ -38,10 +49,91 @@ namespace Sadna_17_B_Test.Tests.AcceptanceTests
         [TestMethod]
         public void TestLogout()
         {
+            SetUp();
+
             Response res = userService.Login(username1, password1);
             userDTO = res.Data as UserDTO;
             Response res2 = userService.Logout(userDTO.AccessToken);
             Assert.IsTrue(res2.Success);
+        }
+
+        [TestMethod]
+        public void TestGuestEntry()
+        {
+            SetUp();
+
+            Response res1 = userService.GuestEntry();
+            Assert.IsTrue(res1.Success);
+            Assert.IsNotNull(res1.Data);
+        }
+
+        [TestMethod]
+        public void TestGuestLogout()
+        {
+            SetUp();
+
+            Response res1 = userService.GuestEntry();
+            userDTO = res1.Data as UserDTO;
+            Response res2 = userService.GuestExit(userDTO.AccessToken);
+
+            Assert.IsTrue(res2.Success);
+        }
+
+        [TestMethod]
+        public void TestGuestLogoutFail()
+        {
+            SetUp();
+
+            Response res = userService.GuestExit("");
+
+            Assert.IsFalse(res.Success);
+        }
+
+        [TestMethod]
+        public void TestCreateAdmin()
+        {
+            SetUp();
+            username1 = "username1";
+            password1 = "password1";
+            Response res = userService.CreateAdmin(username1, password1);
+
+            Assert.IsTrue(res.Success);
+        }
+
+        [TestMethod]
+        public void TestStoreOpening()
+        {
+            SetUp();
+
+            storeService.CreateStore(name, email, phonenumber, storeDescr, addr, inv);
+            
+            Assert.AreEqual(storeService.GetStoreByName(name)._name, name);
+            Assert.IsNull(storeService.GetStoreByName(email));
+        }
+
+        [TestMethod]
+        public void TestStoreClose()
+        {
+            SetUp();
+
+            storeService.CreateStore(name, email, phonenumber, storeDescr, addr, inv);
+            bool res = storeService.RemoveStore(name);
+            bool res2 = storeService.RemoveStore(email);
+
+            Assert.IsTrue(res);
+            Assert.IsFalse(res2);
+        }
+
+        [TestMethod]
+        public void TestClosedStoreClosing()
+        {
+            SetUp();
+
+            storeService.CreateStore(name, email, phonenumber, storeDescr, addr, inv);
+            bool res = storeService.RemoveStore(name);
+            bool res2 = storeService.RemoveStore(name);
+
+            Assert.IsFalse(res2);
         }
     }
 }
