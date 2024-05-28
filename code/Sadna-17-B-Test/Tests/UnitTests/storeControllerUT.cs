b990afbbdb1b56b68e16c0475f9ccf61cfa6ded1
Daniel Bril <brildan@post.bgu.ac.il>
2024-05-28 23:07:47 +0300

    Store unit tests updates

--- code/Sadna-17-B-Test/Tests/UnitTests/storeControllerUT.cs
+++ code/Sadna-17-B-Test/Tests/UnitTests/storeControllerUT.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Sadna_17_B.DomainLayer.Store;
 using Sadna_17_B.DomainLayer.StoreDom;
 using Sadna_17_B.DomainLayer.User;
 
@@ -16,6 +14,8 @@ namespace Sadna_17_B_Test.Tests.UnitTests
     public class StoreControllerTests
     {
         private StoreController _storeController;
+        private DiscountPolicy _discountPolicy;
+        private Product _product;
         private Inventory _inventory;
         private Subscriber _owner;
 
@@ -24,13 +24,95 @@ namespace Sadna_17_B_Test.Tests.UnitTests
         {
             _storeController = new StoreController();
             _inventory = new Inventory();
+            _discountPolicy = new DiscountPolicy("Test Policy");
+            _product = new Product("Test Product", 100, "Category", 5, "Good product");
+
         }
+
+        [TestMethod]
+        public void TestAddAndRemoveDiscount()
+        {
+            // Arrange
+            var discount = new VisibleDiscount(DateTime.Now, DateTime.Now.AddDays(10), new Discount_Percentage(10));
+
+            // Act
+            var addResult = _discountPolicy.add_discount(discount);
+            var removeResult = _discountPolicy.remove_discount(discount);
+
+            // Assert
+            Assert.IsTrue(addResult);
+            Assert.IsTrue(removeResult);
+        }
+
+        [TestMethod]
+        public void TestCalculateDiscount_Percentage()
+        {
+            // Arrange
+            var discount = new VisibleDiscount(DateTime.Now, DateTime.Now.AddDays(10), new Discount_Percentage(10));
+            _discountPolicy.add_discount(discount);
+            _discountPolicy.add_product(discount, _product.Id);
+
+            // Act
+            var discountedPrice = _discountPolicy.calculate_discount(_product.Id, _product.Price);
+
+            // Assert
+            Assert.AreEqual(90, discountedPrice, 0.01);
+        }
+
+        [TestMethod]
+        public void TestCalculateDiscount_Flat()
+        {
+            // Arrange
+            var discount = new HiddenDiscount(DateTime.Now, DateTime.Now.AddDays(10), new Discount_Flat(20));
+            _discountPolicy.add_discount(discount);
+            _discountPolicy.add_product(discount, _product.Id);
+
+            // Act
+            var discountedPrice = _discountPolicy.calculate_discount(_product.Id, _product.Price);
+
+            // Assert
+            Assert.AreEqual(80, discountedPrice, 0.01);
+        }
+
+        [TestMethod]
+        public void TestCalculateDiscount_Member()
+        {
+            Product product3 = new Product("Test Product", 100, "Category", 5, "Good product");
+
+            // Arrange
+            var discount = new VisibleDiscount(DateTime.Now, DateTime.Now.AddDays(10), new Discount_Member());
+            _discountPolicy.add_discount(discount);
+            _discountPolicy.add_product(discount, product3.Id);
+
+            // Act
+            var discountedPrice = _discountPolicy.calculate_discount(product3.Id, product3.Price);
+
+            // Assert
+            Assert.AreEqual(70, discountedPrice, 0.01); // Assuming the membership discount applied correctly
+        }
+
+        [TestMethod]
+        public void TestMultipleDiscounts()
+        {
+            // Arrange
+            var discount1 = new VisibleDiscount(DateTime.Now, DateTime.Now.AddDays(10), new Discount_Percentage(10));
+            var discount2 = new HiddenDiscount(DateTime.Now, DateTime.Now.AddDays(10), new Discount_Flat(5));
+            _discountPolicy.add_discount(discount1);
+            _discountPolicy.add_discount(discount2);
+            _discountPolicy.add_product(discount1, _product.Id);
+            _discountPolicy.add_product(discount2, _product.Id);
+
+            // Act
+            var discountedPrice = _discountPolicy.calculate_discount(_product.Id, _product.Price);
+
+            // Assert
+            Assert.AreEqual(85, discountedPrice, 0.01); // 10% discount applied first, then 5 flat discount
+        }
+
 
         [TestMethod]
         public void TestCreateStore()
         {
-            SetUp();
-
             // Act
             var storeBuilder = _storeController.GetStoreBuilder()
                                 .SetName("Test Store")
@@ -101,6 +183,7 @@ namespace Sadna_17_B_Test.Tests.UnitTests
         [TestMethod]
         public void TestRemoveStore()
         {
+            _storeController.clearAllStores();
             // Arrange
             var storeBuilder = _storeController.GetStoreBuilder()
                                 .SetName("Test Store")
@@ -113,11 +196,12 @@ namespace Sadna_17_B_Test.Tests.UnitTests
             _storeController.AddStore(store);
 
             // Act
-            //_storeController.RemoveStore(store);
+            //_storeController.CloseStore(storeID);
             var result = _storeController.GetStoreByName("Test Store");
 
             // Assert
-            Assert.IsNull(result);
+            Assert.AreNotEqual(result._name, "Test Store");
+            //Assert.IsNull(result);
         }
 
         [TestMethod]
