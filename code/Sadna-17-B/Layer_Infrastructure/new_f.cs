91b61ee2acf4d74e4b7391a1c166a44e49a3d94e
golyuval <golyuvalg@gmail.com>
2024-07-15 03:04:26 +0300

    ,

--- code/Sadna-17-B/Layer_Infrastructure/new_f.cs
+++ code/Sadna-17-B/Layer_Infrastructure/new_f.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Web;
 
 namespace Sadna_17_B.Layer_Infrastructure
 {
-    public class @new
+    public class c
     {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+    }
+
+
+    public class a: c
+    {
+        [JsonPropertyName("class a")]
+        public string type { get; set; }
+    }
+
+    public class b : c
+    {
+        [JsonPropertyName("class b")]
+        public string type { get; set; }
     }
 }
